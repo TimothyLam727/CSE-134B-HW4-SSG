32753a720c7f588461db4d187465836f1ed5880a
Timothy <w4lam@ucsd.edu>
2025-03-02 16:53:11 -0800

    added 11ty SSG page

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+    // Copy styles folder to the output directory.
+    eleventyConfig.addPassthroughCopy("src/styles");
+    // Optionally, if you have images or scripts:
+    eleventyConfig.addPassthroughCopy("src/img");
+    // eleventyConfig.addPassthroughCopy("src/scripts");
+    
+    return {
+      dir: {
+        input: "src",
+        output: "dist"
+      }
+    };
+  };
+  
